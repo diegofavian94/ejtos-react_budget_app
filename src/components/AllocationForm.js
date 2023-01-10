ac91532438934100170e98f0dbe5b3262aa61823
diegofavian94 <diegofavian94@gmail.com>
2023-01-10 17:59:58 -0500

    Project finished

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -1,1 +1,81 @@
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
 
+const AllocationForm = (props) => {
+    const { dispatch,remaining  } = useContext(AppContext);
+
+    const [name, setName] = useState('');
+    const [cost, setCost] = useState('');
+    const [action, setAction] = useState('');
+
+    const submitEvent = () => {
+
+            if(cost > remaining) {
+                alert("El valor no puede exceder los fondos restantes $"+remaining);
+                setCost("");
+                return;
+            }
+
+        const expense = {
+            name: name,
+            cost: parseInt(cost),
+        };
+        if(action === "Reduce") {
+            dispatch({
+                type: 'RED_EXPENSE',
+                payload: expense,
+            });
+        } else {
+                dispatch({
+                    type: 'ADD_EXPENSE',
+                    payload: expense,
+                });
+            }
+    };
+
+    return (
+        <div>
+            <div className='row'>
+
+            <div className="input-group mb-3" style={{ marginLeft: '2rem' }}>
+                    <div className="input-group-prepend">
+                <label className="input-group-text" htmlFor="inputGroupSelect01">Departamento</label>
+                </div>
+                  <select className="custom-select" id="inputGroupSelect01" onChange={(event) => setName(event.target.value)}>
+                        <option defaultValue>Elegir...</option>
+                        <option value="Marketing" name="marketing"> Mercado</option>
+                <option value="Sales" name="sales">Ventas</option>
+                <option value="Finance" name="finance">Finanzas</option>
+                <option value="HR" name="hr">HR</option>
+                <option value="IT" name="it">IT</option>
+                <option value="Admin" name="admin">Admin</option>
+                  </select>
+
+                    <div className="input-group-prepend" style={{ marginLeft: '2rem' }}>
+                <label className="input-group-text" htmlFor="inputGroupSelect02">Asignar</label>
+                </div>
+                  <select className="custom-select" id="inputGroupSelect02" onChange={(event) => setAction(event.target.value)}>
+                        <option defaultValue value="Add" name="Add">Añadir</option>
+                <option value="Reduce" name="Reduce">Reducir</option>
+                  </select>
+
+                    <input
+                        required='required'
+                        type='number'
+                        id='cost'
+                        value={cost}
+                        style={{ marginLeft: '2rem' , size: 10}}
+                        onChange={(event) => setCost(event.target.value)}>
+                        </input>
+
+                    <button className="btn btn-primary" onClick={submitEvent} style={{ marginLeft: '2rem' }}>
+                        Guardar
+                    </button>
+                </div>
+                </div>
+
+        </div>
+    );
+};
+
+export default AllocationForm;
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,1 +1,28 @@
+import React, { useContext } from 'react';
+import ExpenseItem from './ExpenseItem';
+import { AppContext } from '../context/AppContext';
 
+const ExpenseList = () => {
+    const { expenses } = useContext(AppContext);
+
+    return (
+        <table className='table'>
+              <thead className="thead-light">
+            <tr>
+              <th scope="col">Departamento</th>
+              <th scope="col">Presupuesto asignado</th>
+              <th scope="col">Incrementar en 10</th>
+              <th scope="col">Eliminar</th>
+            </tr>
+          </thead>
+            <tbody>
+            {console.log(expenses)}
+            {expenses.map((expense) => (
+                <ExpenseItem id={expense.id} key={expense.id} name={expense.name} cost={expense.cost} />
+            ))}
+            </tbody>
+        </table>
+    );
+};
+
+export default ExpenseList;
